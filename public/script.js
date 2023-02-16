5db499012c0e10554fd35593dbbe19bafa381432
Pyae Phyo Han <pyaephyohan2200@gmail.com>
2023-02-16 13:39:18 +0630

    modify code design

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const fetchData = async () => {
   const apiUrlFromLs = localStorage.getItem("apiUrl");
   if (apiUrlFromLs) {
-    const response = await fetch(`${apiUrlFromLs}/users`);
+    const response = await fetch(apiUrlFromLs + "/users");
     const data = await response.json();
     console.log(data);
   } else {
